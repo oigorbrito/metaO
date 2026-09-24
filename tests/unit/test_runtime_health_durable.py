583a0469c2982dd5c639426ea8b7c94b929fe4de
oigorbrito <oigorbrito@gmail.com>
2026-09-24 06:10:34 -0300

    test: fix durable health fixture formatting

--- tests/unit/test_runtime_health_durable.py
+++ tests/unit/test_runtime_health_durable.py
@@ -26,7 +26,8 @@ from metao.runtime_health import (
     RuntimeHealthState,
     RuntimeHealthTracker,
 )
-from metao.sqlite_runtime_health import SQLiteRuntimeHealthStore`nfrom metao.failure_origin import BoundFailureOriginEvidence, FailureOrigin, FactualFailureOutcome
+from metao.sqlite_runtime_health import SQLiteRuntimeHealthStore
+from metao.failure_origin import BoundFailureOriginEvidence, FailureOrigin, FactualFailureOutcome
 from metao.strategy import OrchestratorStatus
 
 
